c1733b892cf827a6438b073bc6595321ea5b2236
Muhammad Amir <muhammad@withplum.com>
2022-08-28 13:21:32 +0200

    added reactjs app with dependencies
    added reactjs app with dependencies

--- backend/main.py
+++ backend/main.py
@@ -11,9 +11,25 @@ def greeting():
 @app.get("/board")
 def get_board():
     board_data = {
-        'tasks': {},
-        'columns': {},
-        'columnOrder': []
+        'tasks': {
+            'task-1': {'id': 'task-1', 'content': 'create user'},
+            'task-2': {'id': 'task-2', 'content': 'update user'},
+            'task-3': {'id': 'task-3', 'content': 'view user'},
+            'task-4': {'id': 'task-4', 'content': 'delete user'},
+        },
+        'columns': {
+            'column-1': {
+                'id': 'column-1', 
+                'title': 'TO DO',
+                'taskIds': ['task-1', 'task-2']
+            },
+            'column-2': {
+                'id': 'column-2', 
+                'title': 'TO DO',
+                'taskIds': ['task-2', 'task-3']
+            },
+        },
+        'columnOrder': ['column-1', 'column-2']
     }
-    return {"board": {}}
+    return {"board": board_data}
 
---
+++ frontend/src/components/Board.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+    display: flex;
+    `
+
+function Board(){
+    return (
+        <Container>
+            Board
+        </Container>
+    );
+}
+
+
+export default Board;
